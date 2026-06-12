9203514db05ac467160a6bd91ced0f3c1a0d1955
etzba <supoort@etzba.com>
2026-06-12 18:47:04 +0200

    change vite config base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/wwwvite/",
+  base: "/",
   plugins: [
     react(),
     commonjs(),
