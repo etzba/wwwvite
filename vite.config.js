0a04776841b7cec8ab09edf62c61065b4e58fad2
etzba <supoort@etzba.com>
2026-06-13 15:05:49 +0200

    set vite base with url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import commonjs from "vite-plugin-commonjs";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://etzba.github.io/wwwvite/",
   plugins: [
     react(),
     commonjs(),
