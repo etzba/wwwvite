8850b48e45717bb1dfa59c62a3014b32e3e0da1e
etzba <supoort@etzba.com>
2026-07-07 11:49:08 +0200

    Chore(Cleanup): Minimal setup with less components - prepare restructure

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { DownArrow, RightArrow } from "./Arrows";
+import "../styles/sidebar.css";
+
+const Sidebar = ({
+  items,
+  activeId,
+  onNavigate,
+  collapsed,
+  onToggleCollapse,
+}) => {
+  const [openMenus, setOpenMenus] = useState({});
+  const toggleMenu = (id) =>
+    setOpenMenus((prev) => ({ ...prev, [id]: !prev[id] }));
+
+  return (
+    <>
+      <nav className="sidebar">
+        <ul>
+          {items.map((item, index) =>
+            item.children ? (
+              <li key={index}>
+                <Link
+                  className={`nav-item leaf ${openMenus[item.id] ? "open" : ""} ${item.children.some((c) => c.id === activeId) ? "active-parent" : ""}`}
+                  onClick={() => toggleMenu(item.path)}
+                  to={item.path}
+                  key={item.id}
+                >
+                  <span>{item.title}</span>
+                  {openMenus[item.id] && !collapsed ? (
+                    <RightArrow />
+                  ) : (
+                    <DownArrow />
+                  )}
+                </Link>
+                {openMenus[item.id] && !collapsed && (
+                  <div className="sub-menu">
+                    <ul>
+                      {item.children.map((child, childIndex) => (
+                        <li key={childIndex}>
+                          <Link
+                            to={item.path + "/" + child.path}
+                            key={child.id}
+                            className={`nav-item child ${activeId === child.id ? "active" : ""}`}
+                            onClick={() =>
+                              onNavigate(item.path + "/" + child.path)
+                            }
+                          >
+                            <span>{child.title}</span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </li>
+            ) : (
+              <Link
+                key={item.id}
+                to={item.path}
+                className={`nav-item leaf ${activeId === item.id ? "active-parent" : ""}`}
+                onClick={() => onNavigate(item.path)}
+              >
+                {!collapsed && <span>{item.title}</span>}
+              </Link>
+            ),
+          )}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Sidebar;
--- src/pages/Docs/WelcomeData.jsx
+++ src/pages/Docs/WelcomeData.jsx
@@ -1,41 +1,33 @@
-import InfoBlock from "../../components/InfoBlock";
 import "../../styles/layout.css";
 
 const Welcome = () => {
   return (
     <div className="container">
       <section className="section">
-      <h1>Welcome to etz docs!</h1>
-      <h3>A Load and performance testing tool</h3>
-      <p>
-        Welcome to etz docs! Here you'll learn about <code>etz</code>, the load and performance tests tool to measure the performance of of your api service. 
-      </p>
-      <InfoBlock 
-        preText={"The word etzba in Hebrew, used as a measurement unit in biblical times and also means a pointing finger. This tool will measure the duration of http requests to your service and point the finger on potential issues and bottlenecks."}
-      />
-      <p>
-        The tool is compiled and shortly named <code>etz</code> for faster
-        executions in your command line. This cli tool can run in your terminal,
-        shell scripts or in CI pipelines.
-      </p>
-      <p>
-        With additional <code>.yaml</code> files, you can create a complex test
-        scenarios and automate your api \ postgres testing while defining the
-        load on the service by requests per second, test duration and many more
-        options.
-      </p>
-      <InfoBlock
-        title={"Key Features"} 
-        bulletPoints={
-          [
-            "Terminal-native: Run and analyze tests with simple CLI commands",
-            "Scriptable & automatable: Perfect for CI/CD pipelines and custom workflow",
-            "Real-time metrics: Get instant feedback on throughput, latency, and errors",
-            "YAML configuration: Define your test cases with a yaml file and extend it with functions"
-          ]
-        }
-        />
-        </section>
+        <h1>Welcome page</h1>
+        <p>
+          We minimize output to make it easy to restructure when needed
+        </p>
+        <p>
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+        </p>
+        <p>
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+        </p>
+        <h3>Minimal output</h3>
+        <p>
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+          We minimize output to make it easy to restructure when needed
+        </p>
+      </section>
     </div>
   );
 }
